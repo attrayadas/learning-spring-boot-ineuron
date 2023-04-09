8ae19aeb9a3cd90169e9d585af2b912c05f5f487
Attraya Das <attrayaghoshdas@gmail.com>
2023-04-10 01:51:45 +0530

    Rename BootProj16-DAOUsingJDBCTemplateDirectMethods

--- BootProj16-DAOUsingJDBCTemplateDirectMethods/src/main/java/in/ineuron/BootProj16DaoUsingJdbcTemplateDirectMethodsApplication.java
+++ BootProj16-DAOUsingJDBCTemplateDirectMethods/src/main/java/in/ineuron/BootProj16DaoUsingJdbcTemplateDirectMethodsApplication.java
@@ -11,11 +11,11 @@ import org.springframework.context.ConfigurableApplicationContext;
 import in.ineuron.service.IEmployeeMangementService;
 
 @SpringBootApplication
-public class BootProj14DaoUsingJdbcTemplateDirectMethodsApplication {
+public class BootProj16DaoUsingJdbcTemplateDirectMethodsApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = SpringApplication
-				.run(BootProj14DaoUsingJdbcTemplateDirectMethodsApplication.class, args);
+				.run(BootProj16DaoUsingJdbcTemplateDirectMethodsApplication.class, args);
 
 		IEmployeeMangementService service = applicationContext.getBean(IEmployeeMangementService.class);
 		List<Map<String, Object>> employeeDetailsByDesignation = service.getEmployeeDetailsByDesignation("captain",
