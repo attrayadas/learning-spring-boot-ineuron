480b879cd6ffff7b6a4257463b4938e31137edd6
Attraya Das <attrayaghoshdas@gmail.com>
2023-05-24 20:34:32 +0530

    Update SpringRest10-RestTemplateConsumerApp

--- SpringRest10-RestTemplateConsumerApp/src/main/java/in/ineuron/SpringRest10ConsumerAppApplication.java
+++ SpringRest10-RestTemplateConsumerApp/src/main/java/in/ineuron/SpringRest10ConsumerAppApplication.java
@@ -13,3 +13,25 @@ public class SpringRest10ConsumerAppApplication {
 	}
 
 }
+
+//Output:
+//	ActorServiceConsumingRunner.run()
+//	ResponseBody                :: Good Night Attraya
+//	Response Status Code Value  :: 200
+//	Response Status Code        :: OK
+//	*************************************************
+//	ActorServiceConsumingRunner_GettingJsonData.run()
+//	ResponseBody                :: Actor(aid=7, name=nitin, age=29.0, type=Teacher)
+//	ResponseStatus Code Value   :: 200
+//	ResponseStatus Code         :: OK
+//	*************************************************
+//	ActorServiceConsumingRunner_PathVariable.run()
+//	ResponseBody                :: Good Night Dhoni; Associated id is 10
+//	Response Status Code Value  :: 200
+//	Response Status Code        :: OK
+//	*************************************************
+//	ActorServiceConsumingRunner_PostingJsonData.run()
+//	ResponseBody                :: Actor data is: Actor(aid=3, name=Sachin Tendulkar, age=54.0, type=batsman)
+//	Response Status Code Value  :: 201
+//	Response Status Code        :: CREATED
+//	*************************************************
