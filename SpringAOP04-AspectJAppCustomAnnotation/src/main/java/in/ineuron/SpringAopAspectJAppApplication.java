cc51ea352f2969d1b0d05a2750c2b0e4eeed714e
Attraya Das <attrayaghoshdas@gmail.com>
2023-05-27 01:08:38 +0530

    Add SpringAOP04-AspectJAppCustomAnnotation

---
+++ SpringAOP04-AspectJAppCustomAnnotation/src/main/java/in/ineuron/SpringAopAspectJAppApplication.java
@@ -0,0 +1,18 @@
+package in.ineuron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopAspectJAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAopAspectJAppApplication.class, args);
+	}
+
+}
+
+//Output:
+//	***Before Transaction***
+//	Employee Object is saved...
+//	***Before Transaction***
