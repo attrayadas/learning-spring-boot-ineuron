3085b256b005a3e2e4d624e0db5559902fdca033
Attraya Das <attrayaghoshdas@gmail.com>
2023-05-28 00:41:49 +0530

    Add SpringDataJPA13-PostgreSQLApp

---
+++ SpringDataJPA13-PostgreSQLApp/src/main/java/in/ineuron/SpringDataJpa12H2DatabaseAppApplication.java
@@ -0,0 +1,13 @@
+package in.ineuron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpa12H2DatabaseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpa12H2DatabaseAppApplication.class, args);
+	}
+
+}
