d4fd8a46784fd5b3356aa28a3866ad69098113fe
Attraya Das <attrayaghoshdas@gmail.com>
2023-06-01 01:20:56 +0530

    Add Stock-Price-Service

---
+++ Stock-Price-Service/src/main/java/in/ineuron/repository/StockRepository.java
@@ -0,0 +1,11 @@
+package in.ineuron.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import in.ineuron.entity.StockPrice;
+
+public interface StockRepository extends JpaRepository<StockPrice, Integer> {
+
+	public StockPrice findByCompanyName(String companyName);
+
+}
---
+++ Stock-Price-Service/src/main/java/in/ineuron/service/StockPriceServiceImpl.java
@@ -0,0 +1,26 @@
+package in.ineuron.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import in.ineuron.entity.StockPrice;
+import in.ineuron.exception.StockNotFoundException;
+import in.ineuron.repository.StockRepository;
+
+@Service
+public class StockPriceServiceImpl implements IStockPriceService {
+
+	@Autowired
+	private StockRepository repo;
+
+	@Override
+	public Double findByCompanyName(String companyName) {
+
+		StockPrice stockPrice = repo.findByCompanyName(companyName);
+		if (stockPrice == null) {
+			throw new StockNotFoundException("Company not available");
+		}
+		return stockPrice.getCompanyPrice();
+	}
+
+}
