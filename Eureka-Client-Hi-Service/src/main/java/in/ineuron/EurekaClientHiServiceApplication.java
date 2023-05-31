8153695cd10746f8923b6d5d2a8ce6cdb7380d0c
Attraya Das <attrayaghoshdas@gmail.com>
2023-06-01 00:11:22 +0530

    Update Eureka-Client-Hi-Service

--- Eureka-Client-Hi-Service/src/main/java/in/ineuron/EurekaClientHiServiceApplication.java
+++ Eureka-Client-Hi-Service/src/main/java/in/ineuron/EurekaClientHiServiceApplication.java
@@ -3,9 +3,11 @@ package in.ineuron;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class EurekaClientHiServiceApplication {
 
 	public static void main(String[] args) {
