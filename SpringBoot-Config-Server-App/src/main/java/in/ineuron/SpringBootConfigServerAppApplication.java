02b5919ae42266fd8b6bdcad612175ab2d37c656
Attraya Das <attrayaghoshdas@gmail.com>
2023-06-04 15:04:30 +0530

    Add SpringBoot-Config-Server-App

---
+++ SpringBoot-Config-Server-App/src/main/java/in/ineuron/SpringBootConfigServerAppApplication.java
@@ -0,0 +1,15 @@
+package in.ineuron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringBootConfigServerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConfigServerAppApplication.class, args);
+	}
+
+}
