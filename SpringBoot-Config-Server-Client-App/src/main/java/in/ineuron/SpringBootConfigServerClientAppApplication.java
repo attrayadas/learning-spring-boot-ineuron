f15599a29f3ebc63ec8a4b973b9fe514d4a8caf4
Attraya Das <attrayaghoshdas@gmail.com>
2023-06-04 15:04:56 +0530

    Add SpringBoot-Config-Server-Client-App

---
+++ SpringBoot-Config-Server-Client-App/src/main/java/in/ineuron/SpringBootConfigServerClientAppApplication.java
@@ -0,0 +1,15 @@
+package in.ineuron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConfigServerClientAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConfigServerClientAppApplication.class, args);
+	}
+
+}
+
+// Make sure SpringBoot-Admin-Server in running
