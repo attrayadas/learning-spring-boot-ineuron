3d6d7c68c89f064502155bd97cb81e8c8abd7008
Attraya Das <attrayaghoshdas@gmail.com>
2023-06-06 02:11:40 +0530

    Add MS-Redis-Cache-App

---
+++ MS-Redis-Cache-App/src/test/java/in/ineuron/SbRedisCacheAppApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ineuron;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbRedisCacheAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
