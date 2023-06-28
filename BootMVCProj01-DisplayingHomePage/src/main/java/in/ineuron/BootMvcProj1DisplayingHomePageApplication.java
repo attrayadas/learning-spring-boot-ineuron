0ac66f7126457f3a70c8178fa18b6fb9b861f444
Attraya Das <attrayaghoshdas@gmail.com>
2023-06-28 16:34:38 +0530

    Update BootMVCProj01-DisplayingHomePage

--- BootMVCProj01-DisplayingHomePage/src/main/java/in/ineuron/BootMvcProj1DisplayingHomePageApplication.java
+++ BootMVCProj01-DisplayingHomePage/src/main/java/in/ineuron/BootMvcProj1DisplayingHomePageApplication.java
@@ -2,13 +2,23 @@ package in.ineuron;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class BootMvcProj1DisplayingHomePageApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootMvcProj1DisplayingHomePageApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(BootMvcProj1DisplayingHomePageApplication.class,
+				args);
+		System.out.println("Container name is: " + applicationContext.getClass().getName());
+		
 
 	}
 
 }
+
+//Output:
+//	Container name is: org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext
+
+//Request:
+//	http://localhost:8080/FirstApp/index.jsp
--- BootMVCProj01-DisplayingHomePage/src/main/java/in/ineuron/controller/ShowHomeController.java
+++ BootMVCProj01-DisplayingHomePage/src/main/java/in/ineuron/controller/ShowHomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class ShowHomeController {
 
-	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
+	@RequestMapping(value = "/welcome",method = RequestMethod.GET)
 	public String showHomePage() {
 		return "home";
 	}
