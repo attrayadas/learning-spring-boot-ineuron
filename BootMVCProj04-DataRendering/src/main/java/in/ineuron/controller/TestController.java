ba19930cd7acad64cd4dd5dee6f56defcd95da67
Attraya Das <attrayaghoshdas@gmail.com>
2023-07-02 00:14:06 +0530

    Update BootMVCProj04-DataRendering

--- BootMVCProj04-DataRendering/src/main/java/in/ineuron/controller/TestController.java
+++ BootMVCProj04-DataRendering/src/main/java/in/ineuron/controller/TestController.java
@@ -1,9 +1,13 @@
 package in.ineuron.controller;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import in.ineuron.model.Employee;
 
 @Controller
 public class TestController {
@@ -23,23 +27,22 @@ public class TestController {
 		String[] countryNames = new String[] { "IND", "USA", "Russia", "UK" };
 		model.put("countryNames", countryNames);
 
-//		List<String> subjetList = List.of("java", "jee", "spring", "springboot", "hibernate");
-//		model.put("subjectList", subjetList);
-//
-//		Set<Long> mobileSet = Set.of(9999777888L, 6665554443L, 888777654L);
-//		model.put("mobileSet", mobileSet);
-//
-//		Map<String, Number> idsMap = Map.of("adhar", 456783, "voterId", 543456L, "panNo", 4534524L);
-//		model.put("idsMap", idsMap);
-//
-//		List<Employee> empList = List.of(new Employee(10, "sachin", "batsman", 23456.0),
-//				new Employee(7, "dhoni", "keeper", 453456.0), new Employee(18, "kohli", "captain", 3456756.0)
-//
-//		);
-//		model.put("empList", empList);
-//
-//		Employee employee = new Employee(45, "rohith", "batsman", 434567.0);
-//		model.put("employee", employee);
+		List<String> subjetList = List.of("java", "jee", "spring", "springboot", "hibernate");
+		model.put("subjectList", subjetList);
+
+		Set<Long> mobileSet = Set.of(9999777888L, 6665554443L, 888777654L);
+		model.put("mobileSet", mobileSet);
+
+		Map<String, Number> idsMap = Map.of("aadhaar", 456783, "voterId", 543456L, "panNo", 4534524L);
+		model.put("idsMap", idsMap);
+
+		List<Employee> empList = List.of(new Employee(10, "sachin", "batsman", 23456.0),
+				new Employee(7, "dhoni", "keeper", 453456.0), new Employee(18, "kohli", "captain", 3456756.0));
+		model.put("empList", empList);
+
+		Employee employee = new Employee(45, "rohith", "batsman", 434567.0);
+		model.put("employee", employee);
+
 		return "show_report";
 	}
 }
--- BootMVCProj04-DataRendering/src/main/webapp/WEB-INF/pages/show_report.jsp
+++ BootMVCProj04-DataRendering/src/main/webapp/WEB-INF/pages/show_report.jsp
@@ -14,7 +14,8 @@
 		<h1>name is :: ${name}</h1>
 		<h1>age is :: ${age}</h1>
 		<h1>address is :: ${address}</h1>
-		<hr>
+		<hr/>
+		
 		<h1 style='color: red; text-align: center;'>Displaying Array
 			values</h1>
 		<b>CountryNames</b><br/>
@@ -22,6 +23,58 @@
 			${country}<br/>
 		</c:forEach>
 		<hr/>
+		
+		<h1 style='color: red; text-align: center;'>Displaying List
+			values</h1>
+		<b>Subject Names</b><br/>
+		<c:forEach var="subject" items="${subjectList}">
+			${subject}<br/>
+		</c:forEach>
+		<hr/>
+		
+		<h1 style='color: red; text-align: center;'>Displaying Set
+			values</h1>
+		<b>Mobile Numbers</b><br/>
+		<c:forEach var="mobile" items="${mobileSet}">
+			${mobile}<br/>
+		</c:forEach>
+		<hr/>
+		
+		<h1 style='color: red; text-align: center;'>Displaying Map
+			values</h1>
+		<b>ID Info</b><br/>
+		<c:forEach var="id" items="${idsMap}">
+			${id.key} ----> ${id.value }<br/>
+		</c:forEach>
+		<hr/>
+
+		<h1 style='color: red;'>Displaying List of DTO</h1>
+		<table border='1'>
+			<tr>
+				<th>ENO</th>
+				<th>ENAME</th>
+				<th>EDESG</th>
+				<th>SALARY</th>
+			</tr>
+
+			<c:forEach items="${empList}" var="emp">
+				<tr>
+					<td>${emp.eno}</td>
+					<td>${emp.ename}</td>
+					<td>${emp.edesg}</td>
+					<td>${emp.salary}</td>
+				</tr>
+			</c:forEach>
+		</table>
+		<hr/>
+		
+		<h1 style='color:red; text-align: center;'>
+			Reading Model Object Data
+		</h1>
+		<b>EMPNO     IS :: ${employee.eno}</b><br/>
+		<b>EMPNAME   IS :: ${employee.ename}</b><br/>
+		<b>EMPDESG   IS :: ${employee.edesg}</b><br/>
+		<b>EMPSALARY IS :: ${employee.salary}</b>
 	</center>
 </body>
 </html>
