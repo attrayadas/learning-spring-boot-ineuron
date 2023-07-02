1e011329c41beff35a25da696707347c18b3f964
Attraya Das <attrayaghoshdas@gmail.com>
2023-07-02 14:03:36 +0530

    Add BootMVCProj05-DataBindingAppUsingHTMLForms

---
+++ BootMVCProj05-DataBindingAppUsingHTMLForms/src/main/java/in/ineuron/model/Employee.java
@@ -0,0 +1,15 @@
+package in.ineuron.model;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+	private Integer eno;
+	private String ename;
+	private String edesg;
+	private Double salary;
+	
+	public Employee(){
+		System.out.println("Zero Param Constructor is used by DS");
+	}
+}
